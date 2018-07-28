d577052243e5dbb149bbc6c7fc30644b199a5cd5
jayanam <jayanam.games@gmail.com>
2018-07-28 19:51:11 +0200

    Refactoring

--- ptm_mapping_op.py
+++ ptm_mapping_op.py
@@ -1,18 +1,14 @@
 import bpy
 from bpy.types import Operator
-
-
+ 
 class PTM_MappingOperator(Operator):
     bl_idname = "object.ptm_mapping_op"
     bl_label = "Map textures"
     bl_description = "Connect PBR textures to the Principled shader" 
     bl_options = {'REGISTER', 'UNDO'} 
-       
-    @classmethod
-    def poll(cls, context):        
-        return True
     
     def add_link(self, tree, node, node2, output_name, input_name, non_color_data = False):
+        
         tree.links.new(node.outputs[output_name], node2.inputs[input_name])
         
         if(hasattr(node, "color_space")):
@@ -23,37 +19,40 @@ class PTM_MappingOperator(Operator):
     
     def create_normal_map(self, tree):
         return tree.nodes.new('ShaderNodeNormalMap')
-        
-         
+             
     def execute(self, context):
         
         tree = context.space_data.node_tree
-        shader_node = [n for n in tree.nodes if n.type == "BSDF_PRINCIPLED"][0]
         
-        # shader_node = tree.nodes["
+        shader_nodes  = [n for n in tree.nodes if n.type == "BSDF_PRINCIPLED"]
+        
         texture_nodes = [n for n in tree.nodes if n.type == "TEX_IMAGE"]
+        
+        if(len(shader_nodes) == 0 or len(texture_nodes) == 0):
+            return {'CANCELLED'} 
+        
+        shader_node = shader_nodes[0] 
             
         for node in texture_nodes:
             
             lower_name = node.image.name.lower()
             
-            if any(name in lower_name for name in ["diffuse", "albedo"] ):
-                            
+            if any(name in lower_name for name in ["diffuse", "albedo"]):            
                 self.add_link(tree, node, shader_node, "Color", "Base Color")
                 
-            elif("metal" in lower_name):
+            elif("metallic" in lower_name):
                 self.add_link(tree, node, shader_node, "Color", "Metallic", True)
                 
-            elif("rough" in lower_name):
+            elif("roughness" in lower_name):
                 self.add_link(tree, node, shader_node, "Color", "Roughness", True)
                    
             elif("normal" in lower_name):
                 
                 if(len(node.outputs["Color"].links) == 0):
+                    
                     normal_node = self.create_normal_map(tree)
                     
                     self.add_link(tree, node, normal_node, "Color", "Color", True)
                     self.add_link(tree, normal_node, shader_node, "Normal", "Normal")
                 
-
         return {'FINISHED'}
--- ptm_panel.py
+++ ptm_panel.py
@@ -1,21 +1,44 @@
 import bpy
 from bpy.types import Panel
 
-from . ptm_mapping_op import PTM_MappingOperator
+from . ptm_mapping_op   import PTM_MappingOperator
+
+# Add this when icon_value works again in Blender 2.8
+# from . ptm_icon_manager import PTM_IconMgr
 
 class PTM_Panel(Panel):
     bl_space_type = "NODE_EDITOR"
     bl_region_type = "TOOLS"
-    bl_label = "Texture Mapping"
+    bl_label = "PTM"
     bl_category = "PTM"
     
     def draw(self, context):
         
         layout = self.layout
         scene = context.scene
-
-                                
+                            
         # Mapping operator
         row = layout.row()
-        row.operator('object.ptm_mapping_op', text="Map textures", icon='MOD_BEVEL')
+        row.operator('object.ptm_mapping_op', text="Map textures", icon="FACESEL_HLT")
+        
+        layout.row().separator()
+        
+        # Alignment (4 columns)
+        row = layout.row()
+        split = row.split(0.2)
+        
+        col = split.column()
+        col.operator('object.ptm_align_op', text="←", icon="NODE").align = 0
+        
+        col = split.column()
+        col.operator('object.ptm_align_op', text="→", icon="NODE").align = 1
+        
+        col = split.column()
+        col.operator('object.ptm_align_op', text="↑", icon="NODE").align = 2
+        
+        col = split.column()
+        col.operator('object.ptm_align_op', text="↓", icon="NODE").align = 3
+        
+        col = split.column()
+        col.operator('object.ptm_align_op', text="↔", icon="NODE").align = 4
  
