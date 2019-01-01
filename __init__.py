3245f35f733f078dfc645f320617fec7278f9139
jayanam <jayanam.games@gmail.com>
2019-01-01 23:53:20 +0100

    Panel on the right side

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "PBR Texture Mapper",
     "description": "Map image nodes from tools like Substance Painter to principled shader",
     "author": "Jayanam",
-    "version": (0, 9, 0, 1),
+    "version": (0, 9, 0, 2),
     "blender": (2, 80, 0),
     "location": "NodeEditor",
     "category": "Material"}
@@ -14,24 +14,18 @@ from bpy.props import *
 
 from . ptm_mapping_op    import PTM_MappingOperator
 from . ptm_align_op      import PTM_AlignOperator
-from . ptm_icon_manager  import PTM_IconMgr
 from . ptm_panel         import PTM_Panel
 
 addon_keymaps = []
 iconMgr = None
 
 def register():
-   iconMgr = PTM_IconMgr()
-   iconMgr.load()
-   
    bpy.utils.register_class(PTM_MappingOperator)
    bpy.utils.register_class(PTM_AlignOperator)
    bpy.utils.register_class(PTM_Panel)
-   
 
     
 def unregister():
-   iconMgr.unload()
    bpy.utils.unregister_class(PTM_MappingOperator)
    bpy.utils.unregister_class(PTM_AlignOperator)
    bpy.utils.unregister_class(PTM_Panel)
--- ptm_panel.py
+++ ptm_panel.py
@@ -4,22 +4,22 @@ from bpy.types import Panel
 from . ptm_mapping_op   import PTM_MappingOperator
 
 # Add this when icon_value works again in Blender 2.8
-# from . ptm_icon_manager import PTM_IconMgr
+from . ptm_icon_manager import PTM_IconMgr
 
 class PTM_Panel(Panel):
     bl_space_type = "NODE_EDITOR"
-    bl_region_type = "TOOLS"
+    bl_region_type = "UI"
     bl_label = "PTM"
     bl_category = "PTM"
     
     def draw(self, context):
         
         layout = self.layout
-        scene = context.scene
+        scene = context.scene      
                             
         # Mapping operator
         row = layout.row()
-        row.operator('object.ptm_mapping_op', text="Map textures", icon="FACESEL_HLT")
+        row.operator('object.ptm_mapping_op', text="Map textures", icon="NODE")
         
         layout.row().separator()
         
